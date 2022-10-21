a931486bf8fd4a33723d62e1cbf8ffb876ccf6bf
Neelabh <neelabhsomani1818@gmail.com>
2022-10-21 18:46:49 +0530

    This project has been discontinued because the API is taking too long to respond. Gonna build my own Speech-Text recognition model

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
+#Import dependencies
 import sys
+import glob
 import requests
+import sounddevice as sd 
 from config import API_KEY
-#Record the file
+from scipy.io.wavfile import write
 
-#Upload the file
-upload_url = 'https://api.assemblyai.com/v2/upload'
-filename = 'sample1.wav'
-def read_file(filename, chunk_size=5242880):
-    with open(filename, 'rb') as _file:
-        while True:
-            data = _file.read(chunk_size)
-            if not data:
-                break
-            yield data
+#Recording an audio
+def record(duration, name_of_audio):
+    fs = 44100
+    print("-"*30,"Recording","-"*30)
+    record = sd.rec(int(duration*fs), samplerate=fs, channels=1)
+    sd.wait()
+    write('{}.wav'.format(name_of_audio),fs, record)
+    print("-"*30,"Recording finished","-"*30)
+    return record
 
-headers = {'authorization': API_KEY}
-response = requests.post(upload_url,
-                        headers=headers,
-                        data=read_file(filename))
-audio_url = response.json()['upload_url']
-#Transcribe the file
-endpoint_url = "https://api.assemblyai.com/v2/transcript"
-json = { "audio_url": "https://bit.ly/3yxKEIY" }
-response = requests.post(endpoint_url, json=json, headers=headers)
-response_id = response.json()['id']
-#Checking if transcription is ready or not
-polling_url = endpoint_url + '/' + response_id
-polling_result = requests.get(polling_url, headers=headers)
-print(polling_result.json())
-#Save transcript
+#Upload the recording.
+def upload(audio_file):
+    upload_url = 'https://api.assemblyai.com/v2/upload'
+    filename = audio_file
+    def read_file(filename, chunk_size=5242880):
+        with open(filename, 'rb') as _file:
+            while True:
+                data = _file.read(chunk_size)
+                if not data:
+                    break
+                yield data
+    headers = {'authorization': API_KEY}
+    response = requests.post(upload_url,
+                            headers=headers,
+                            data=read_file(filename))
+    audio_url = response.json()['upload_url']
+
+#Trancribing the file.
+def transcribe():
+    endpoint_url = "https://api.assemblyai.com/v2/transcript"
+    json = { "audio_url": "https://bit.ly/3yxKEIY" }
+    response = requests.post(endpoint_url, json=json, headers=headers)
+    response_id = response.json()['id']
